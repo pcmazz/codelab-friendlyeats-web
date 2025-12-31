1a0d91234776bc5dfb781dac0b560d5539f083f9
Phil Mazz <pcmaslow@gmail.com>
2025-12-30 23:33:49 -0600

    Butt Watcher

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,7 +1,6 @@
-import {initializeApp} from '@angular/fire/app'
-import {getMessaging} from '@angular/fire/messaging'
-
-const firebaseConfig = {
+importScripts("https://www.gstatic.com/firebasejs/9.6.11/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.6.11/firebase-messaging-compat.js");
+firebase.initializeApp({
   apiKey: 'AIzaSyC9mtlPIlHQbvBEdMfJ9tkBpgQg4w5XXIA',
   authDomain: 'friendlychat-mazz.firebaseapp.com',
   projectId: 'friendlychat-mazz',
@@ -9,23 +8,6 @@ const firebaseConfig = {
   messagingSenderId: '1029208851330',
   appId: '1:1029208851330:web:baab23e0429de6c859c59b',
   measurementId: 'G-HMSNQS15B1'
-};
+});
 
-// 2. Initialize Firebase and Messaging in the worker
-const app = initializeApp(firebaseConfig);
-const messaging = getMessaging(app);
-
-// 3. (Optional) Custom Background Message Handler
-// FCM handles standard notifications automatically if the payload has a 'notification' property.
-// Use this for data-only messages or custom logic.
-/*onBackgroundMessage(messaging, (payload) => {
-  console.log('[firebase-messaging-sw.js] Background message received:', payload);
-
-  const notificationTitle = payload.notification?.title || 'New Message';
-  const notificationOptions = {
-    body: payload.notification?.body || 'You have a new update.',
-    icon: '/assets/icons/icon-128x128.png'
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});*/
+const messaging = firebase.messaging();
