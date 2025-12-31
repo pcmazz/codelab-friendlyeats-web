e4f6ab9b8646ea811b891dce1b22c241c0714d4c
Phil Mazz <pcmaslow@gmail.com>
2025-12-30 18:58:17 -0600

    fuck firestore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,14 +11,12 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-mazz","appId":"1:1029208851330:web:baab23e0429de6c859c59b","storageBucket":"friendlychat-mazz.firebasestorage.app","apiKey":"AIzaSyC9mtlPIlHQbvBEdMfJ9tkBpgQg4w5XXIA","authDomain":"friendlychat-mazz.firebaseapp.com","messagingSenderId":"1029208851330","projectNumber":"1029208851330","version":"2"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideFunctions(() => getFunctions()),
+    provideStorage(() => getStorage()),
+    provideMessaging(() => getMessaging()),
+    provideRouter(routes)
   ],
 };
